77a99ad1711c07af2f331f58fb6d9ab330618b0d
Bruno Diaz <brunjo12@hotmail.com>
2022-04-26 15:29:18 -0500

    actualizacion jwtUtils

--- src/main/java/com/demo/shop/web/config/JwtUtils.java
+++ src/main/java/com/demo/shop/web/config/JwtUtils.java
@@ -13,7 +13,8 @@ public class JwtUtils {
     private static final String KEY = "TOKEN";
 
     public String generateToken(UserDetails userDetails){
-        return Jwts.builder().setSubject(userDetails.getUsername())
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, KEY).compact();
